9d3a325534dd38739ba1c868c974a2771ea2891a
Febiana D. Anjani <febianadahliaa@gmail.com>
2025-02-01 14:12:37 +0800

    Modify konstruksi function

--- R/lapus_adhb_konstruksi.R
+++ R/lapus_adhb_konstruksi.R
@@ -1,27 +1,25 @@
-#' Memuat lapangan usaha F. Konstruksi
+#' Lapus Konstruksi - ADHB
 #'
-#' Fungsi ini menjalankan alur penghitungan PDRB menurut lapangan usaha konstruksi.
+#' Fungsi ini menjalankan alur penghitungan PDRB ADHB menurut lapangan usaha konstruksi.
 #'
-#' @param x Angka pertama (numeric)
-#' @param y Angka kedua (numeric)
-#' @return Hasil penjumlahan dari x dan y
 #' @export
 #'
 #' @examples
-#' konstruksi_adhk()
-konstruksi_adhk <- function() {
-  cat("\n", "Konstruksi", "\n")
-  cat("Kegiatan Konstruksi terdiri atas konstruksi umum dan khusus Gedung dan bangunan sipil baik berupa pekerjaan baru, perbaikan, penambahan, dan perubahan, pendirian fabrikasi bangunan di lokasi suatu proyek, konstruksi yang bersifat sementara, termasuk juga rehabilitasi bangunan Gedung dan bangunan sipil, persewaan peralatan konstruksi dengan operatornya, serta work in progress seperti konstruksi proyek multiyear.")
-  cat("\n Pendekatan Konstruksi kabkot: PMTB Bangunan atau metode secara langsung \n")
+#' konstruksi_adhb()
+
+konstruksi_adhb <- function() {
+  cat("\nKONSTRUKSI\n")
+  cat("Kegiatan Konstruksi terdiri atas konstruksi umum dan khusus Gedung dan bangunan sipil baik berupa pekerjaan baru, perbaikan, penambahan, dan perubahan, pendirian fabrikasi bangunan di lokasi suatu proyek, konstruksi yang bersifat sementara, termasuk juga rehabilitasi bangunan Gedung dan bangunan sipil, persewaan peralatan konstruksi dengan operatornya, serta work in progress seperti konstruksi proyek multiyear.\n\n")
+  cat("Pendekatan Konstruksi kabkot: PMTB Bangunan atau metode secara langsung \n")
   cat("Sumber Data:\n")
   cat("1. Pemerintah: Realisasi APBN Bangunan, Realisasi APBD Bangunan, Realisasi APBDes Bangunan, dan Realisasi APBD Provinsi Bangunan dari BPKAD;\n")
   cat("2. Swasta: PMA Bangunan, PMDN Bangunan, Perkiraan Nilai Bangunan IMB-PBG;\n")
   cat("3. Rumah Tangga: Perkiraan Nilai Bangunan IMB-PBG;\n")
   cat("4. IHPB Konstruksi level Provinsi;\n")
   cat("5. Rasio NTB Konstruksi dari Tabel I-O Provinsi;\n")
-  cat("6. Rasio PMTB Bangunan\n")
+  cat("6. Rasio PMTB Bangunan\n\n")
 
-  nilai_bangunan <- as.numeric(readline("\n Masukkan Perkiraan Nilai Bangunan/Output (Luas Bangunan x Harga Bangunan per m2) hasil Pendataan PMTB Kuesioner IMB-PBG (dalam Jutaan Rp): ")) #input 1
+  nilai_bangunan <- as.numeric(readline("Masukkan Perkiraan Nilai Bangunan/Output (Luas Bangunan x Harga Bangunan per m2) hasil Pendataan PMTB Kuesioner IMB-PBG (dalam Jutaan Rp): ")) #input 1
   cat("Output Konstruksi Rumah Tangga adalah: ", nilai_bangunan, "\n\n")
 
   nilai_pma <- as.numeric(readline("Masukkan Nilai/Output Penanaman Modal Asing (PMA) dari isian Kuesioner PMTB LKPM (PMA x Nilai Tukar Rupiah terhadap Dolar dalam Jutaan Rp): ")) #input 2
@@ -40,15 +38,15 @@ konstruksi_adhk <- function() {
   ihpb_konstruksi <- as.numeric(readline("Masukkan Indeks Konstruksi Nasional dengan tahun dasar 2010, (tahun dasar Indeks Konstruksi Nasinal 2005=100, 2010=100, dan 2018=100): ")) #input 11
   pendapatan_konstruksi <- as.numeric(readline("Masukkan Pendapatan Konstruksi Indonesia (cek Publikasi Konstruksi Indonesia): ")) #input 12
   ntb_t <- as.numeric(readline("Masukkan NTB Konstruksi Indonesia tahun ini (t) (cek Publikasi Konstruksi Indonesia): ")) #input 13
-  ntb_t_1 <- as.numeric(readline("Masukkan NTB Konstruksi Indonesia tahun sebelumnya (t-1) (cek Publikasi Konstruksi Indonesia): \n")) #input 14
+  ntb_t_1 <- as.numeric(readline("Masukkan NTB Konstruksi Indonesia tahun sebelumnya (t-1) (cek Publikasi Konstruksi Indonesia): ")) #input 14
   cat("Rasio NTB Konstruksi Indonesia adalah: ", ntb_t/pendapatan_konstruksi, "\n")
-  cat("Laju NTB Konstruksi Indonesia adalah: ", ntb_t/ntb_t_1*100-100, "\n")
-  rasio_ntb_konstruksi <- as.numeric(readline("Masukkan Rasio NTB Konstruksi Provinsi Anda dari Tabel I-O 2016 (Rasio Konstruksi SE2016 x Laju NTB Konstruksi Indonesia \n\n")) #input 15
+  cat("Laju NTB Konstruksi Indonesia adalah: ", ntb_t/ntb_t_1*100-100, "\n\n")
 
-  output_adhb <- nilai_bangunan + nilai_pma + nilai_pmdnn + nilai_ibmpbg + nilai_apbn_kppn + nilai_apbd_lpse + nilai_apbd_bpkad + nilai_apbdes_dpmd
+  rasio_ntb_konstruksi <- as.numeric(readline("Masukkan Rasio NTB Konstruksi Provinsi Anda dari Tabel I-O 2016 (Rasio Konstruksi SE2016 x Laju NTB Konstruksi Indonesia: ")) #input 15
+  output_adhb <- nilai_bangunan + nilai_pma + nilai_pmdn + nilai_ibmpbg + nilai_apbn_kppn + nilai_apbd_lpse + nilai_apbd_bpkad + nilai_apbdes_dpmd
   ntb_konstruksi_adhb <- output_adhb*rasio_ntb_konstruksi/100
   cat("Output Konstruksi Total ADHB adalah: ", output_adhb, "\n")
-  cat("NTB Konstruksi Total ADHB adalah: ", ntb_konstruksi_adhb, "\n")
+  cat("NTB Konstruksi Total ADHB adalah: ", ntb_konstruksi_adhb, "\n\n")
 
   # output_adhk <- output_adhb/ihpb_konstruksi
   # ntb_konstruksi_adhk <- output_adhb/ihpb_konstruksi*rasio_ntb_konstruksi/100
@@ -60,9 +58,12 @@ konstruksi_adhk <- function() {
   cat("NTB ADHB Lapangan Usaha ini (dalam jutaan Rp) adalah: ", ntb_total, "\n")
 
   hasil <- data.frame(
-    Tipe = ifelse(opsi == 1, "ADHK", "ADHB"),
-    Id_Lapus = id_lapus,
-    Nama_Lapus = nama_pilihan,
+    #Tipe = ifelse(opsi == 1, "ADHK", "ADHB"),
+    #Id_Lapus = id_lapus,
+    #Nama_Lapus = nama_pilihan,
+    Tipe = "ADHB",
+    Id_Lapus = 6,
+    Nama_Lapus = "Konstruksi",
     Output_Konstruksi = output_adhb,
     Adjustment_NTB = adjustment_ntb,
     NTB_Total = ntb_total
